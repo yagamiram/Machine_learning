b1e7f6ffc745d68130aeb5c3a80ce12e0b8a05de
Yagamiram Elric TK <sriram.ssnit@gmail.com>
2016-02-27 20:29:42 -0600

    Create logistic_regression.py

---
+++ logistic_regression.py
@@ -0,0 +1,8 @@
+The below links will give some insight on logistic regression but no derviation and no concept on how
+to get a learning model.
+
+http://gormanalysis.com/logistic-regression-fundamentals/
+https://codesachin.wordpress.com/2015/08/16/logistic-regression-for-dummies/
+
+code for logistic regression:
+http://nbviewer.jupyter.org/gist/vietjtnguyen/6655020
