9d1e1196be595ff1d4bc77ba8d420091275ea1b0
Yagamiram Elric TK <sriram.ssnit@gmail.com>
2016-02-27 23:39:42 -0600

    Create neural_network.py

---
+++ neural_network.py
@@ -0,0 +1,8 @@
+In 11 lines of Python code:
+http://iamtrask.github.io/2015/07/12/basic-python-network/
+
+Learn neural networks:
+https://medium.com/learning-new-stuff/how-to-learn-neural-networks-758b78f2736e#.wwt8ytib8
+
+Implement Neuaral networks:
+http://peterroelants.github.io/posts/neural_network_implementation_part01/
